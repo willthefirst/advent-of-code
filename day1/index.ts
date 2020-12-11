db013da6aae19b9010e2d807a87dd725d7f1ab1c
Will Moritz <will@crowdfavorite.com>
2020-12-11 09:42:50 -0800

    solved day1 part1

--- day1/index.ts
+++ day1/index.ts
@@ -1,16 +1,39 @@
 import { read } from "../utils";
 
 const strToNums = (str: string): number[] => {
-    return str.split('\n').map(line => parseInt(line, 10));
-}
+	return str.split("\n").map((line) => parseInt(line, 10));
+};
 
 const parsePuzzleInput = async (filename: string): Promise<number[]> => {
-    return read(filename).then(strToNums);
-}
+	return read(filename).then(strToNums);
+};
 
-const solve = async () => {
-    const nums = await parsePuzzleInput("day1");
-    console.log(nums);
-}
+const solvePart1 = async (): Promise<number | null> => {
+	const nums = await parsePuzzleInput("day1");
+	nums.sort();
 
-solve();
+	let i = 0,
+		j = nums.length;
+
+	let solution: number | null = null;
+
+	// Compare first and last nums in sorted array
+	// If sum is smaller than 2020, increase index of first num
+	// Else if sum is larger than 2020, decrease index of last num
+	while (i < j && !solution) {
+		let a = nums[i],
+			b = nums[j];
+		const sum = a + b;
+
+		if (sum === 2020) {
+			solution = a * b;
+		} else if (sum < 2020) {
+			i++;
+		} else {
+			j--;
+		}
+	}
+	return solution;
+};
+
+solvePart1().then((solution) => console.log(solution));
