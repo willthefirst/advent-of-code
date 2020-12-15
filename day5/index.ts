d94b401136f88525e4efdf59e2d4c13dfa04e596
Will Moritz <will@crowdfavorite.com>
2020-12-15 15:58:56 -0800

    5.1: solved

--- day5/index.ts
+++ day5/index.ts
@@ -2,13 +2,38 @@ import { read } from "../utils";
 
 const parseInput = (filename: string) => {
 	return read(filename).then((string: string) => {
-		return string;
+		return string.split("\n");
 	});
 };
 
+const binToInt = (bin: string): number => {
+	return parseInt(bin, 2);
+};
+
+const anyToBinary = (str: string, zero: string, one: string) => {
+	return str.replace(new RegExp(zero, "g"), "0").replace(new RegExp(one, "g"), "1");
+};
+
+const calculateRow = (directions: string): number => {
+	return binToInt(anyToBinary(directions.slice(0, 7), "F", "B"));
+};
+
+const calculateColumn = (directions: string): number => {
+	return binToInt(anyToBinary(directions.slice(7), "L", "R"));
+};
+
+const calculateSeatId = (row: number, col: number) => {
+	return row * 8 + col;
+};
+
+const directionsToSeatId = (directions: string): number => {
+	return calculateSeatId(calculateRow(directions), calculateColumn(directions));
+};
+
 const solvePart1 = async () => {
-	const puzzle = await parseInput("day5");
-	return puzzle;
+	const seats = await parseInput("day5");
+	const seatIds = seats.map(directionsToSeatId);
+	return Math.max(...seatIds);
 };
 
 const solvePart2 = async () => {
