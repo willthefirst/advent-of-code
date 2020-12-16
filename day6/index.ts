ed57da9d00b3f3195a7febbf64a59c301eafb87a
Will Moritz <will@crowdfavorite.com>
2020-12-15 17:13:03 -0800

    6..2: solved

--- day6/index.ts
+++ day6/index.ts
@@ -6,23 +6,41 @@ const parseInput = async (filename: string) => {
 };
 
 const countUniqueChars = (str: string): number => {
-	return([...new Set(str)].length);
-}
+	return [...new Set(str)].length;
+};
+
+const countAgreements = (group: string[]): number => {
+	const numMembers = group.length;
+	const yesses = group.join("");
+	const record = [...new Set(yesses)];
+	return record
+		.map((char): number => {
+			const occurrences = yesses.match(new RegExp(char, "g"));
+			if (occurrences && occurrences.length === numMembers) {
+				return 1;
+			}
+			return 0;
+		})
+		.reduce((acc, curr) => acc + curr);
+};
 
 const solvePart1 = async () => {
 	const groups = await parseInput("day6");
 	return groups
-			.map(group => {
-				return countUniqueChars(
-					group.replace(/\n/g, "")
-				)
-			}).reduce((acc, curr) => acc + curr);
+		.map((group) => {
+			return countUniqueChars(group.replace(/\n/g, ""));
+		})
+		.reduce((acc, curr) => acc + curr);
 };
 
 const solvePart2 = async () => {
-	const puzzle = await parseInput("day6");
-	return puzzle;
+	const groups = await parseInput("day6");
+	return groups
+		.map((group) => {
+			return countAgreements(group.split("\n"));
+		})
+		.reduce((acc, curr) => acc + curr);
 };
 
 solvePart1().then((result) => console.log("Part 1 solution:", result));
-// solvePart2().then((result) => console.log("Part 2 solution:", result));
+solvePart2().then((result) => console.log("Part 2 solution:", result));
