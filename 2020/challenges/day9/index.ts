67a6d7751c97be39cd64468d698a86549fe94ba4
Will Moritz <will@crowdfavorite.com>
2020-12-17 14:14:11 -0800

    9.2: solved

--- 2020/challenges/day9/index.ts
+++ 2020/challenges/day9/index.ts
@@ -1,4 +1,4 @@
-import { findAncestor } from "typescript";
+import { findAncestor, ScriptTarget } from "typescript";
 import { read } from "../../utils";
 
 const parseInput = async (filename: string) => {
@@ -34,10 +34,62 @@ const solvePart1 = async () => {
 	return findError(data, 25);
 };
 
+let sumFromIndex = (index: number, range: number, nums: number[], cachedFn: Function): number => {
+	if (range < 0) {
+		return 0;
+	}
+	return nums.slice(index, index + range + 1).reduce((acc, curr) => acc + curr);
+};
+
+const memoizeSumFromIndex = (fn: Function) => {
+	const sumPairCache: any = {};
+
+	return (index: number, range: number, nums: number[], cachedFn: Function) => {
+		if (index in sumPairCache && range === 1) {
+			return sumPairCache[index];
+		}
+
+		if (range < 1) {
+			return nums[index];
+		}
+
+		let result = cachedFn(index, range - 1, nums, cachedFn) + nums[index + range];
+
+		if (range === 1) {
+			// Only cache pairs
+			sumPairCache[index] = result;
+		}
+
+		return result;
+	};
+};
+
+sumFromIndex = memoizeSumFromIndex(sumFromIndex);
+
+const minPlusMax = (array: number[]) => {
+	return Math.min(...array) + Math.max(...array);
+};
+
+let findEncryptionWeakness = (data: number[], preambleLength: number): number | null => {
+	const error = findError(data, preambleLength);
+	for (let range = 1; range < data.length; range++) {
+		for (let i = 0; i < data.length; i++) {
+			if (i + range > data.length) {
+				continue;
+			}
+			const sumOfRange = sumFromIndex(i, range, data, sumFromIndex);
+			if (sumOfRange === error) {
+				return minPlusMax(data.slice(i, i + range + 1));
+			}
+		}
+	}
+	return null;
+};
+
 const solvePart2 = async () => {
 	const data = await parseInput("day9");
-	return data;
+	return findEncryptionWeakness(data, 25);
 };
 
 solvePart1().then((result) => console.log("Part 1 solution:", result));
-// solvePart2().then((result) => console.log("Part 2 solution:", result));
+solvePart2().then((result) => console.log("Part 2 solution:", result));
