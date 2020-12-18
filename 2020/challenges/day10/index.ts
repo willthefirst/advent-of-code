76688b51694445ce0b98dfee83192bd84690a772
Will Moritz <will@crowdfavorite.com>
2020-12-18 09:48:55 -0800

    10.2 working iteratively but still out of memory

--- 2020/challenges/day10/index.ts
+++ 2020/challenges/day10/index.ts
@@ -38,42 +38,97 @@ const memoize = (fn: Function): any => {
 	const cache: any = {};
 	return function (arr: any[]) {
 		const key = arr[0];
-		
-		if (cache[key]) {
+
+		if (key in cache) {
 			return cache[key];
 		}
-		
+
 		const result = fn(arr);
+
 		cache[key] = result;
 		console.log(cache);
 		return result;
 	};
 };
 
-let getAllArrangements = (adapters: number[]): number[][] => {
-	const arrangements = [adapters];
-	for (let i = 1; i < adapters.length - 1; i++) {
-		let arrangement = Array.from(adapters);
-		for (let j = i; j < arrangement.length - 1; ) {
-			if (arrangement[i + 1] - arrangement[i - 1] <= 3) {
-				arrangement.splice(i, 1);
-				getAllArrangements(arrangement.slice(i)).forEach((arr) => {
-					arrangements.push(arrangement.slice(0, i).concat(arr));
-				});
-			} else {
-				break;
+const getAllPaths = (adapters: number[]): {} => {
+	const paths: any = {};
+	for (let i = 0; i < adapters.length; i++) {
+		let pathsFromI = [];
+		for (let j = 3; j >= 1; j--) {
+			// Push the serial sequence once
+			if (adapters[i + j] - adapters[i] <= 3) {
+				pathsFromI.push(j + i);
 			}
 		}
+		// This gives us all travel paths between i and i + 3;
+		paths[i] = pathsFromI.sort();
 	}
-	return arrangements;
+	return paths;
 };
 
-getAllArrangements = memoize(getAllArrangements);
+const newArrayForEach = (arr: any[], newItems: any[]): any[][] => {
+	return newItems.map(val => arr.concat(val))
+}
+
+const allPossibleArrays = (arraysToAddTo: any[][], newItems: any[]): any[][] => {
+	return arraysToAddTo.map((item) => {
+		return newArrayForEach(item, newItems)
+	}).flat();
+} 
+
+let getArrangementsFromPaths = (adapters: number[], paths: any): number[][] => {
+	let unfinishedMaps: number[][] = [[0]];
+	let finishedMaps: number[][] = [];
+
+	const lastIndex = Object.keys(paths).length - 1;
+	console.log(paths);
+
+	while (unfinishedMaps.length > 0) {
+		for (let i = 0; i < unfinishedMaps.length; i++) {
+			const m = unfinishedMaps[i];
+			const nextDestKey: number = m[m.length - 1];
+
+			if (nextDestKey === lastIndex) {
+				unfinishedMaps.splice(i, 1);
+				finishedMaps.push(m);
+				break;
+			}
+
+			const nextDestVals: number[] = paths[nextDestKey];
+			if (nextDestVals.length > 1) {
+				let newPaths = newArrayForEach(m, nextDestVals);
+				unfinishedMaps.splice(i, 1, ...newPaths);
+				break;
+			} else {
+				unfinishedMaps[i] = m.concat(nextDestVals[0]);
+			}
+		}
+		console.log(finishedMaps.length);
+	}
+	return finishedMaps;
+};
+
+// getAllArrangements = memoize(getAllArrangements);
+
+// console.log(getAllArrangements([0, 1, 2, 3, 5]));
+// 0,1,2,3,5
+// 0, ,2,3,5
+// 0,    3,5
+// 0,1,  3,5
+// 0,  2,3,5
+// 0,  2,  5
+
+// 0 [ [ 0, 3 ], [ 0, 2 ], [ 0, 1 ] ]
+// 1 [ [ 1, 3 ], [ 1, 2 ] ]
+// 2 [ [ 2, 5 ], [ 2, 3 ] ]
+// 3 [ [ 3, 5 ] ]
+// 4 []
 
 const solvePart2 = async () => {
-	const adapters = await parseInput("day10_test");
-	const arrangements = getAllArrangements(adapters);
-	console.log(arrangementsp)
+	const adapters = await parseInput("day10");
+	const paths = getAllPaths(adapters);
+	const arrangements = getArrangementsFromPaths(adapters, paths);
 	return arrangements.length;
 };
 
