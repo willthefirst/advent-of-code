c4c0c2702059063557dfc1896dfb5ff74f2bce2b
Will Moritz <will@crowdfavorite.com>
2020-12-20 16:13:20 -0800

    11.2: solved

--- 2020/challenges/day11/index.ts
+++ 2020/challenges/day11/index.ts
@@ -7,6 +7,19 @@ const parseInput = async (filename: string): Promise<string[][]> => {
 	});
 };
 
+const visualizeInput = (seats: string[][]): string => {
+	let seatString = "";
+	seats.forEach((row) => {
+		row.forEach((seat) => {
+			seatString = seatString + seat;
+		});
+		seatString = seatString + "\n";
+	});
+	return seatString;
+};
+
+// Count methods
+
 const countAdjacentOccupied = (x: number, y: number, seats: string[][]): number => {
 	let count = 0;
 
@@ -38,39 +51,89 @@ const countAdjacentOccupied = (x: number, y: number, seats: string[][]): number 
 	return count;
 };
 
-const updateSeats = (seats: string[][]): string[][] => {
+const getVisibleSeatAlongVector = (
+	originX: number,
+	originY: number,
+	changeX: number,
+	changeY: number,
+	seats: string[][]
+): number => {
+	const nextX = originX + changeX;
+	const nextY = originY + changeY;
+
+	// Stop if beyond limits
+	if (nextX < 0 || nextX > seats[0].length - 1 || nextY < 0 || nextY > seats.length - 1) {
+		return 0;
+	}
+
+	const seat = seats[nextY][nextX];
+	if (seat === "#") {
+		return 1;
+	} else if (seat === "L") {
+		return 0;
+	} else {
+		return getVisibleSeatAlongVector(nextX, nextY, changeX, changeY, seats);
+	}
+};
+
+const countVisibleOccupied = (x: number, y: number, seats: string[][]) => {
+	const vectors = [
+		[-1, -1],
+		[0, -1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+		[0, 1],
+		[-1, 1],
+		[-1, 0]
+	];
+
+	return vectors
+		.map((v) => getVisibleSeatAlongVector(x, y, v[0], v[1], seats))
+		.reduce((acc, curr) => acc + curr);
+};
+
+// General methods
+
+const nextSeatState = (state: string, numOccupied: number, numOccupiedLimit: number): string => {
+	if (numOccupied >= numOccupiedLimit) {
+		state = "L";
+	} else if (numOccupied === 0) {
+		state = "#";
+	}
+	return state;
+};
+
+const countTotalOccupied = (seats: string[][]) => {
+	return seats.flat().filter((s) => s === "#").length;
+};
+
+const updateSeatsByCountMethod = (
+	seats: string[][],
+	countMethod: Function,
+	numOccupiedLimit: number
+): string[][] => {
 	return seats.map((row: string[], y: number, originalSeats: string[][]): string[] => {
 		return row.map((seat: string, x: number): string => {
 			if (seat === ".") {
 				return seat;
 			}
-
-			// Count adjacent occupied seats
-			const numOfAdjacentOccupied: number = countAdjacentOccupied(x, y, originalSeats);
-
-			if (seat === "L" && numOfAdjacentOccupied === 0) {
-				return "#";
-			}
-
-			if (seat === "#" && numOfAdjacentOccupied >= 4) {
-				return "L";
-			}
-
-			return seat;
+			const numOccupied: number = countMethod(x, y, originalSeats);
+			return nextSeatState(seat, numOccupied, numOccupiedLimit);
 		});
 	});
 };
 
-const countOccupied = (seats: string[][]) => {
-	return seats.flat().filter((s) => s === "#").length;
-};
-
-const seatGuests = (oldSeats: string[][], oldNumOccupied: number): string[][] => {
-	const newSeats = updateSeats(oldSeats);
-	const newNumOccupied = countOccupied(newSeats);
-
+const seatGuests = (
+	oldSeats: string[][],
+	oldNumOccupied: number,
+	countMethod: Function,
+	numOccupiedLimit: number
+): string[][] => {
+	const newSeats = updateSeatsByCountMethod(oldSeats, countMethod, numOccupiedLimit);
+	const newNumOccupied = countTotalOccupied(newSeats);
 	if (oldNumOccupied !== newNumOccupied) {
-		return seatGuests(newSeats, newNumOccupied);
+		return seatGuests(newSeats, newNumOccupied, countMethod, numOccupiedLimit);
 	}
 
 	return newSeats;
@@ -78,14 +141,15 @@ const seatGuests = (oldSeats: string[][], oldNumOccupied: number): string[][] =>
 
 const solvePart1 = async () => {
 	const seats = await parseInput("day11");
-	const finalSeating = seatGuests(seats, 0);
-	return countOccupied(finalSeating);
+	const finalSeating = seatGuests(seats, 0, countAdjacentOccupied, 4);
+	return countTotalOccupied(finalSeating);
 };
 
 const solvePart2 = async () => {
-	const puzzle = await parseInput("day11_test");
-	return puzzle;
+	const seats = await parseInput("day11");
+	const finalSeating = seatGuests(seats, 0, countVisibleOccupied, 5);
+	return countTotalOccupied(finalSeating);
 };
 
-solvePart1().then((result) => console.log("Part 1 solution:" + result));
-// solvePart2().then((result) => console.log("Part 2 solution:", result));©
+solvePart1().then((result) => console.log("Part 1 solution:", result));
+solvePart2().then((result) => console.log("Part 2 solution:", result));
