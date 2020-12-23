64d80381d3d79b37daeac1e014235c3628c0da2d
Will Moritz <will@crowdfavorite.com>
2020-12-23 09:14:45 -0800

    15.2: different approach, not helping

--- 2020/challenges/day15/index.ts
+++ 2020/challenges/day15/index.ts
@@ -1,3 +1,4 @@
+import { createVoidZero } from "typescript";
 import { read } from "../../utils";
 
 const parseInput = async (filename: string) => {
@@ -6,18 +7,40 @@ const parseInput = async (filename: string) => {
 };
 
 const getNthNumber = (n: number, nums: number[]): number => {
-	while (nums.length < n) {
-		const lastTurnNum = nums.length;
-		const last = nums[lastTurnNum - 1];
-		const lastIndexOfLast = nums.slice(0, nums.length - 1).lastIndexOf(last);
+	const latest = new Map();
+	const penultimate = new Map();
+	
+	// 0:1	3:6	6:3
+	// penultimtate
+	// 0:1	3:5
 
-		if (lastIndexOfLast === -1) {
-			nums.push(0);
-		} else {
-			nums.push(lastTurnNum - (lastIndexOfLast + 1));
-		}
+	for (let i = 0; i < nums.length; i++) {
+		latest.set(nums[i], i + 1);
 	}
-	return nums[nums.length - 1];
+
+	let last = nums[nums.length - 1];
+
+	for (let turnNum = nums.length + 1; turnNum <= n; turnNum++) {
+		const previousTurnForNum = penultimate.get(last);
+		if (!previousTurnForNum) {
+			// If this is the first time we've seen this value
+			const lastZero = latest.get(0);
+			penultimate.set(0, lastZero);
+			latest.set(0, turnNum);
+			last = 0;
+		} else {
+			// If we've seen this value before
+			const turnDiff = latest.get(last) - previousTurnForNum;
+			const latestVal = latest.get(turnDiff)
+			if (latestVal) {
+				penultimate.set(turnDiff, latestVal)
+			};
+			latest.set(turnDiff, turnNum);
+			last = turnDiff;
+		}
+		console.log(turnNum, last)
+	}
+	return last;
 };
 
 const solvePart1 = async () => {
@@ -26,8 +49,8 @@ const solvePart1 = async () => {
 };
 
 const solvePart2 = async () => {
-	const numbers = await parseInput("day15_test");
-	return getNthNumber(10, numbers);
+	const numbers = await parseInput("day15");
+	return getNthNumber(30000000, numbers);
 };
 
 // solvePart1().then((result) => console.log("Part 1 solution:", result));
